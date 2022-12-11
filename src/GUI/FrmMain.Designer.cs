e39637bf7808bc52ad8f029b801d6ec933d4b2b3
AaronF11 <aaron.flores7908@alumnos.udg.mx>
2022-12-10 19:41:47 -0600

    Refactorización de carpetas y implementación de primera función

---
+++ src/GUI/FrmMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CS_PY
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.PbStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.BtnMain = new System.Windows.Forms.ToolStripSplitButton();
+            this.writeAndReadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.PbStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 327);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(382, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(52, 20);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // PbStatus
+            // 
+            this.PbStatus.Name = "PbStatus";
+            this.PbStatus.Size = new System.Drawing.Size(100, 18);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.BtnMain,
+            this.toolStripSeparator2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(382, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // BtnMain
+            // 
+            this.BtnMain.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnMain.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.writeAndReadToolStripMenuItem});
+            this.BtnMain.Image = global::CS_PY.Properties.Resources.Python;
+            this.BtnMain.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnMain.Name = "BtnMain";
+            this.BtnMain.Size = new System.Drawing.Size(39, 24);
+            this.BtnMain.Text = "toolStripSplitButton1";
+            this.BtnMain.ButtonClick += new System.EventHandler(this.BtnMain_ButtonClick);
+            // 
+            // writeAndReadToolStripMenuItem
+            // 
+            this.writeAndReadToolStripMenuItem.Image = global::CS_PY.Properties.Resources.One;
+            this.writeAndReadToolStripMenuItem.Name = "writeAndReadToolStripMenuItem";
+            this.writeAndReadToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.writeAndReadToolStripMenuItem.Text = "Write and Read";
+            this.writeAndReadToolStripMenuItem.Click += new System.EventHandler(this.writeAndReadToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar PbStatus;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSplitButton BtnMain;
+        private System.Windows.Forms.ToolStripMenuItem writeAndReadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ src/GUI/FrmMain.cs
@@ -0,0 +1,109 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_PY
+{
+    public partial class FrmMain : Form
+    {
+        //------------------------------------------------------------------------------
+        // Attributes
+        //------------------------------------------------------------------------------
+        private TextBox TextBox;
+        private Button Button;
+        
+        //------------------------------------------------------------------------------
+        // Constructor
+        //------------------------------------------------------------------------------
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        //------------------------------------------------------------------------------
+        // Loading ProgressBar
+        //------------------------------------------------------------------------------
+        private void ProgressBar()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                PbStatus.Value = i;
+                System.Threading.Thread.Sleep(10);
+            }
+            if (PbStatus.Value == 100)
+            {
+                PbStatus.Value = 0;
+            }
+        }
+
+        //------------------------------------------------------------------------------
+        // Declare a ScriptEngine object
+        //------------------------------------------------------------------------------
+        private dynamic Script()
+        {
+            // Create an instance of the ScriptRuntime
+            ScriptRuntime Py = Python.CreateRuntime();
+            // Create a new scope
+            string Path = Application.StartupPath + "\\script.py";
+            // Execute the script
+            dynamic Script = Py.UseFile(Path);
+            return Script;
+        }
+
+        //------------------------------------------------------------------------------------
+        // 1. Write & Read text by textbox to send a py function and return the same string
+        //------------------------------------------------------------------------------------
+        private void writeAndReadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Call the ProgressBar
+            ProgressBar();
+
+            // Creating a new components
+            TextBox = new TextBox //-- Textbox --
+            {
+                Height = 15,
+                Width = 90,
+                Location = new Point(this.Width / 3, this.Height / 2 - 20),
+            };
+            Button = new Button
+            {
+                Height = 32,
+                Width = 64,
+                Location = new Point(this.Width / 3, this.Height / 2 + 20),
+                BackColor = Color.White,
+                Text = "Send"
+            };
+
+            // Add the components to the form
+            Controls.Add(TextBox);
+            Controls.Add(Button);
+
+            // Events
+            Button.Click += delegate
+            {
+                // Call the function
+                string name = Script().Func_Write(TextBox.Text, true).ToString();
+
+                if (name != "")
+                {
+                    // Display the result
+                    MessageBox.Show(name);
+                }
+            };
+        }
+
+        private void BtnMain_ButtonClick(object sender, EventArgs e)
+        {
+            Controls.Remove(TextBox);
+            Controls.Remove(Button);
+        }
+    }
+}
